483c2d4a84c3832f1d3c8c3f8b87120e91117af5
Tunahan Öcal <tunahanocal@Tunahan-MacBook-Air.local>
2026-03-17 13:47:02 +0300

    Add auth system

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,36 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def home(request):
-    return HttpResponse("Ana sayfa çalışıyor 🚀")
+    return render(request, 'accounts/home.html')
 
 def register(request):
-    return HttpResponse("Register sayfası")
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
 
-def login_view(request):
-    return HttpResponse("Login sayfası")
+    return render(request, 'accounts/register.html', {'form': form})
 
-def logout_view(request):
-    return HttpResponse("Logout sayfası")
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
