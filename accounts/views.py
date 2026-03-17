f168eb13ed4108fb277a6441a70086a71d81469a
Tunahan Öcal <tunahanocal@Tunahan-MacBook-Air.local>
2026-03-17 13:34:23 +0300

    Fix missing views

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Ana sayfa çalışıyor 🚀")
+def register(request):
+    return HttpResponse("Register sayfası")
+
+def login_view(request):
+    return HttpResponse("Login sayfası")
+
+def logout_view(request):
+    return HttpResponse("Logout sayfası")
