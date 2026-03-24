a251abca41dc6ec11f05bd6e778769f0d577138d
Tunahan Öcal <tunahanocal@Tunahan-MacBook-Air.local>
2026-03-24 15:03:11 +0300

    11

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100, blank=True)
+    cover_image = models.URLField(blank=True)  # Kitap kapak resmi için URL
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 def __str__(self):
     return f"{self.user.username} Profile"
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 import requests
+from .models import Book
 
 def home(request):
     return render(request, 'accounts/home.html')
@@ -58,4 +59,9 @@ def user_logout(request):
 @login_required
 def profile_view(request):
     return render(request, 'accounts/profile.html')
+
+
+def home(request):
+    books = Book.objects.all().order_by('-created_at')[:8]  # son eklenen 8 kitabı al
+    return render(request, 'accounts/home.html', {'books': books})
 
