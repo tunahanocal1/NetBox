c3531faa999e44d475c75b4ea89164b3c5a7faa8
Tunahan Öcal <tunahanocal@Tunahan-MacBook-Air.local>
2026-06-02 10:16:14 +0300

    11

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_home_page_loads(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+
+class LoginTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword123'
+        )
+
+    def test_login(self):
+        login = self.client.login(
+            username='testuser',
+            password='testpassword123'
+        )
+
+        self.assertTrue(login)
+
+
+class RegisterPageTest(TestCase):
+
+    def test_register_page_loads(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
